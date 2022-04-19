0d656e6e0cb2c7867027b4a5430f7a9eebc42732
laoer536 <1547698569@qq.com>
2022-04-19 20:41:08 +0800

    增加collapse展开方向可自定义

--- src/components/transition/LuCollapse/LuCollapse.vue
+++ src/components/transition/LuCollapse/LuCollapse.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
+import { computed } from "vue";
+
 interface Props {
   show: boolean;
+  collapseDirection?: "toTop" | "toBottom" | "toRight" | "toLeft";
 }
 const props = withDefaults(defineProps<Props>(), {
   show: false,
+  collapseDirection: "toBottom",
+});
+
+const animationOrigin = computed(() => {
+  if (props.collapseDirection === "toTop") return "bottom";
+  else if (props.collapseDirection === "toLeft") return "right";
+  else if (props.collapseDirection === "toRight") return "left";
+  else return "top";
+});
+
+const animation = computed(() => {
+  if (
+    animationOrigin.value.includes("right") ||
+    animationOrigin.value.includes("left")
+  ) {
+    return {
+      start: "scaleX(0)",
+      end: "scaleX(1)",
+    };
+  } else {
+    return {
+      start: "scaleY(0)",
+      end: "scaleY(1)",
+    };
+  }
 });
 </script>
 <template>
@@ -28,13 +56,13 @@ const props = withDefaults(defineProps<Props>(), {
 
 @keyframes collapse {
   0% {
-    transform-origin: top;
-    transform: scaleY(0);
+    transform-origin: v-bind(animationOrigin);
+    transform: v-bind("animation.start");
   }
 
   100% {
-    transform-origin: top;
-    transform: scaleY(1);
+    transform-origin: v-bind(animationOrigin);
+    transform: v-bind("animation.end");
   }
 }
 </style>
