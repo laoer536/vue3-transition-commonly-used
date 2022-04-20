88fcba7ead5e0fbb873803632b06666a57dbc81f
laoer536 <1547698569@qq.com>
2022-04-20 23:47:26 +0800

    数字变化增加可控制是数字增加动画还是数字减少动画

--- src/components/appear/other/LuNumber.vue
+++ src/components/appear/other/LuNumber.vue
@@ -4,25 +4,40 @@ interface Props {
   min: number;
   max: number;
   duration?: number;
+  type?: "increase" | "decrease";
 }
 
 const props = withDefaults(defineProps<Props>(), {
   min: 0,
   max: 15,
-  duration: 1, //设置默认过渡时间
+  duration: 1, //设置默认过渡时间 基本单位秒 s
+  type: "decrease", //默认增加
 });
 
 const intervalDuration = computed(
   () => (props.duration * 1000) / (props.max - props.min)
 );
 
-const currentNumber = ref(props.min);
+const currentNumber = ref<number>(
+  props.type === "increase" ? props.min : props.max
+);
+
 let gradualChange: ReturnType<typeof setInterval>;
 onMounted(() => {
   gradualChange = setInterval(() => {
-    currentNumber.value = currentNumber.value + 1;
-    if (currentNumber.value === props.max) {
-      clearInterval(gradualChange);
+    //增加动画只走这里
+    if (props.type === "increase") {
+      currentNumber.value = currentNumber.value + 1;
+      if (currentNumber.value >= props.max) {
+        clearInterval(gradualChange);
+      }
+    }
+    //减少动画只走这里
+    if (props.type === "decrease") {
+      currentNumber.value = currentNumber.value - 1;
+      if (currentNumber.value <= props.min) {
+        clearInterval(gradualChange);
+      }
     }
   }, intervalDuration.value);
 });
